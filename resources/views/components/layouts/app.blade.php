8484aac386f711b1cbe2cd735e26682203cd808b
Ben Collins <ben.c@afrihost.com>
2026-08-20 20:13:04 +0200

    Feature: Installed Tailwind

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Moesika Lashes by Tania</title>
+</head>
+<body>
+@include('partials.header')
+
+<main>
+    {{ $slot }}
+</main>
+
+@include('partials.footer')
+</body>
+</html>
