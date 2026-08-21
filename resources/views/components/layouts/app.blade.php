292a8e4516c168a801d722122341d9e6f0b73fb8
Ben Collins <ben.c@afrihost.com>
2026-08-21 07:02:03 +0200

    Feature: Added header items as well as installing font-awesome

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>Moesika Lashes by Tania</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,12 @@
-<header class="w-full h-20 shadow-lg bg-linen-cream">
-
+<header class="w-full h-20 shadow-lg bg-linen-cream flex flex-row items-center">
+    <div class="w-[20%] h-full">
+        <img src="{{ asset('images/logo_short.png') }}" alt="logo_short" class="w-25 h-full mx-auto">
+    </div>
+    <nav class="w-[80%] h-full flex flex-row justify-around items-center">
+        <a href="" class="flex flex-row items-center p-6 w-[15%] h-[50%] text-center">Services</a>
+        <a href="" class="flex flex-row items-center p-6 w-[15%] h-[50%] text-center">Training</a>
+        <a href="" class="flex flex-row items-center p-6 w-[15%] h-[50%] text-center">About</a>
+        <a href="" class="flex flex-row items-center p-6 w-[15%] h-[50%] text-center">Gallery</a>
+        <a href="" class="flex flex-row items-center justify-around p-6 w-[15%] h-[50%] text-center rounded-4xl bg-olive-sage text-white"><i class="fab fa-whatsapp text-2xl"></i> Book Now</a>
+    </nav>
 </header>
