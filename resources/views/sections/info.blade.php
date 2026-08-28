4e322c35204fac5488dfba11c36b83f4af1f19dc
Ben Collins <ben.c@afrihost.com>
2026-08-28 08:28:42 +0200

    Feature: added info section as well as added google fonts to the project

---
+++ resources/views/sections/info.blade.php
@@ -0,0 +1,10 @@
+<div class="w-full lg:p-20 p-10 flex justify-center items-center flex-col bg-cover bg-center text-white" style="background-image: linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), url('{{ asset('images/info_bg.jpeg') }}')">
+    <h1 class="lg:w-[70%] w-100% text-center leading-13 montserrat-alternates-semibold">Your destination for professional lash extensions</h1>
+    <p class="montserrat-alternates-extralight mt-2"><i>by Tania</i></p>
+
+    <p class="lg:w-[50%] w-100% p-10 text-center montserrat-alternates-thin">
+        Thoughtful lash artistry, created at your own pace in a serene setting. Every set is
+        individually designed to complement your eyes, your lifestyle, and your desired look.
+        Completely customised, never cookie-cutter.
+    </p>
+</div>
